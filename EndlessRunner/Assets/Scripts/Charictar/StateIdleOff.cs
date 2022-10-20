f297609940ed846b66dfe3ce03d1fec68c74eb80
HAN <98201760+flexinja99@users.noreply.github.com>
2022-10-20 19:29:33 +0900

    remaking
    remaking

--- EndlessRunner/Assets/Scripts/Charictar/StateIdleOff.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class StateMove<T> : StateBase<T> where T : Enum
-{
-    public StateMove(StateMachineBase<T> stateMachineBase, T machineState) : base(stateMachineBase, machineState)
-    {
-
-    }
-}
